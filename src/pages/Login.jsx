735cf12eefa8024c48e5776a206d56dad63933e9
Vivek Shah <95999628+vivekratr@users.noreply.github.com>
2024-02-20 22:38:17 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -46,7 +46,7 @@ const Login = () => {
   
     };
   
-    axios.post('https://cc-zz9j.onrender.com/insertdata', jsonData, {
+    axios.post('http://127.0.0.1:5000/insertdata', jsonData, {
       headers: {
         'Content-Type': 'application/json',
       },
