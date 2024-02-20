27b534df8e4abcad05b11a8fecb4f4ae04b1a04d
Vivek Shah <95999628+vivekratr@users.noreply.github.com>
2024-02-20 22:37:50 +0530

    Update Demo.jsx

--- src/pages/Demo.jsx
+++ src/pages/Demo.jsx
@@ -21,7 +21,7 @@ const Demo = () => {
   
     };
   
-    axios.post('https://cc-zz9j.onrender.com/insertdata', jsonData, {
+    axios.post('http://127.0.0.1:5000/insertdata', jsonData, {
       headers: {
         'Content-Type': 'application/json',
       },
